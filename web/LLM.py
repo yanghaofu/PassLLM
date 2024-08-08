3c1a7a35a19d9af60b6b156605142051dd9131c5
sail-brother <2683390685@qq.com>
2024-08-08 17:20:10 +0800

    update prompt

--- web/LLM.py
+++ web/LLM.py
@@ -150,21 +150,35 @@ def evaluate_password_strength(encrypted_password, encrypted_name, encrypted_ema
         print(f"Decrypted birthday: {birthday}")
 
 
+        # messages = [
+        #     {
+        #         "role": "system",
+        #         "content": (
+        #             "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。"
+        #             "你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，"
+        #             "种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"
+        #         )
+        #     },
+        #     {
+        #         "role": "user",
+        #         # "content": f"请直接给出下面密码的强度（非常强，强，中等，弱，很弱），并解释为什么它强或弱：\n\n密码: {password}"
+        #         "content": f"你现在是一名口令领域的专家，你需要根据给定的口令以及可能附加的用户个人信息，从口令长度、字符种类、字符重复、语义信息、键盘模式、个人信息等多个角度分析口令的安全性。请给出口令强度评级（1-5级），并列出三条左右的评级原因。\n\n用户口令：{password}。"
+        #     },
+        # ]
+
         messages = [
             {
-                "role": "system",
-                "content": (
-                    "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。"
-                    "你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，"
-                    "种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"
-                )
+            "role": "system",
+            "content": (
+            "你现在是一名口令领域的专家，你需要根据给定的口令以及可能附加的用户个人信息，可以从口令长度、字符种类、字符重复、语义信息、键盘模式、个人信息等多个角度分析口令的安全性。"
+            )
             },
             {
-                "role": "user",
-                # "content": f"请直接给出下面密码的强度（非常强，强，中等，弱，很弱），并解释为什么它强或弱：\n\n密码: {password}"
-                "content": f"你现在是一名口令领域的专家，你需要根据给定的口令以及可能附加的用户个人信息，从口令长度、字符种类、字符重复、语义信息、键盘模式、个人信息等多个角度分析口令的安全性。请给出口令强度评级（1-5级），并列出三条左右的评级原因。\n\n用户口令：{password}。"
+            "role": "user",
+            # "content": f"请直接给出下面密码的强度（非常强，强，中等，弱，很弱），并解释为什么它强或弱：\n\n密码: {password}"
+            "content": f"请直接给出下面密码的强度（非常强，强，中等，弱，很弱），并给出四条以内的评级原因。\n\n密码: {password}"
             },
-        ]
+            ]
 
         if name or email or phone or birthday:
             additional_info = (
